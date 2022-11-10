bdff5d84c2ac032c25949e49d4bd5a1bc0a42fb5
dragg54 <ajibolasadiq@yahoo.com>
2022-11-10 04:19:52 +0100

    initial commit

--- db/models/User.js
+++ db/models/User.js
@@ -8,16 +8,6 @@ class UserClass {
         this.password = password;
         this.roleId = roleId
     }
-   create(req, res){
-    const sql = `INSERT INTO user(name, email, phone, password, role_id) VALUES("${this.name}", "${this.email}", "${this.phone}", "${this.password}", ${this.roleId})`;
-    sqlConnection.query(sql, (err, rows, fields) => {
-      if (!err) {
-        return res.status(201).send(rows);
-      } else {
-        return res.status(500).send(err);
-      }
-    });
-   }
 }
 
 module.exports = UserClass
